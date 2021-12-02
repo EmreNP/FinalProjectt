70bd8b03060ade3477a25e471b676cc4555b8c9b
Noble <Noble@DESKTOP-IHDPJJ5>
2021-12-02 12:54:13 +0300

    Add project files.

---
+++ Business/Abstract/IProductService.cs
@@ -0,0 +1,21 @@
+﻿using Core.Utilities.Results;
+using Entities.Concrete;
+using Entities.DtoS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Business.Abstract
+{
+    public interface IProductService
+    {
+        IDataResult<List<Product>> GetAll();
+        IDataResult<List<Product>> GetAllByCategoryId(int Id);
+        IDataResult<List<Product>> GetByUnitPrice(int min, int max);
+        IDataResult<List<ProductDetailDto>> GetProductDetails();
+        IResult Add(Product product);
+        IDataResult<Product> GetById(int Id);
+    }
+}
---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,36 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete.EntityFramework;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             ProductTest();
+            //CategoryTest();
+
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categorManager = new CategoryManager(new EfCategoryDal());
+            foreach (var item in categorManager.GetAll())
+            {
+                Console.WriteLine(item.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
+            ProductManager productManager = new ProductManager(new EfProductDal());
+
+            foreach (var p in productManager.GetProductDetails().Data)
+            {
+                Console.WriteLine(p.ProductName+"        //          "+p.CategoryName);
+            }
+        }
+    }
+}
